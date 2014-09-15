ef6d3557ede15273c22a9ce091e18a2456761177
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-09-15 17:24:28 +0900

    Add Dropwizard demo

---
+++ dropwizard-demo/src/main/java/com/example/dropwizard/App.java
@@ -0,0 +1,27 @@
+package com.example.dropwizard;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class App extends Application<AppConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new App().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<AppConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(AppConfiguration configuration, Environment environment) throws Exception {
+        final UserResource resource = new UserResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName());
+        environment.jersey().register(resource);
+        final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+    }
+}
