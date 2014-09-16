4f9ce3f8d0d26d5afcaab70eaf44e86703c074a7
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-09-16 21:22:17 +0900

    Put banner,txt

--- jersey-demo/src/main/java/com/example/jersey/controllers/UsersController.java
+++ jersey-demo/src/main/java/com/example/jersey/controllers/UsersController.java
@@ -6,13 +6,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
 public class UsersController extends ApplicationController {
     @GET
     @Path("{id}")
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response show(@PathParam("id") String id) {
         return Response.ok(UserResource.DUMMY_USER.toJson()).build();
     }
